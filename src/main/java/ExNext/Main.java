2221bccacce3f0d3262dce4b2de1e6471534fc72
Pjoter K <piotrz9522@gmail.com>
2023-01-21 10:24:35 +0100

    next1

--- src/main/java/ExNext/Main.java
+++ src/main/java/ExNext/Main.java
@@ -1,7 +1,10 @@
 package ExNext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<String> stringList = new ArrayList<>();
     }
 }
